2691d32ba7565fc09e80239ae51277afeaa72d8e
xinnan cao <xinnancao@gmail.com>
2023-01-23 21:34:35 -0500

    added config.js .env

--- config.js
+++ config.js
@@ -1,22 +1,14 @@
 require('dotenv').config();
 module.exports = {
-    development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
-        dialect: "mysql"
-    },
-    test: {
-        username: "root",
-        password: null,
-        database: "testdb",
-        host: "localhost",
-        dialect: "mysql",
-        logging: false
-    },
     production: {
-        use_env_variable: "JAWSDB_URL",
-        dialect: "mysql"
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        ssl: true,
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        }
     }
+    
 };
