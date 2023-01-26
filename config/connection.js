06529070e0a71ab5edb5e61cb930a51a09cb570c
xinnan cao <xinnancao@gmail.com>
2023-01-26 02:04:00 -0500

    added backend login and recipe showing

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const Pool = require('pg').Pool
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-      port: 5432
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  );
-}
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
