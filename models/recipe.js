b37e24087771f5cfd015b33da1f5aaa431f74f0e
xinnan cao <xinnancao@gmail.com>
2023-01-30 20:52:30 -0500

    fix models

--- models/recipe.js
+++ models/recipe.js
@@ -2,8 +2,8 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const User = require('./User');
 
-class Recipe extends Model {}
 
+class Recipe extends Model {}
 Recipe.init(
   {
     id:{
@@ -32,9 +32,6 @@ Recipe.init(
     },
   },
     sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
     modelName: 'recipe',
 });
 
