336eb3d5364fd8dad74e291e816002c590a9e444
xinnan cao <xinnancao@gmail.com>
2023-01-30 20:38:41 -0500

    fix models

--- models/recipe.js
+++ models/recipe.js
@@ -30,46 +30,37 @@ const User = require('./User');
 //     }
 
 // });
+const { Model, DataTypes } = require("sequelize");
 
+class Recipe extends Model {}
 
-class Recipe extends Model {
-    checkPassword(loginPw) {
-      return bcrypt.compareSync(loginPw, this.password);
-    }
-  }
-  Recipe.init(
-    {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
-        ingredients:{
-            type:DataTypes.TEXT,
-            allowNull:false
-        },
-        instructions:{
-            type:DataTypes.TEXT,
-            allowNull:false
-        },
-        userID:{
-            type:DataTypes.INTEGER,
-            references:{
-                model:User,
-                key:'id'
-            }
-        },
+Recipe.init(
+  {
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
-    
-  );
-
-
+	title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+	userID:{
+        type:DataTypes.INTEGER,
+        references:{
+            model:User,
+            key:'id',
+    },
+  },
+});
 
 module.exports = Recipe;
-
-
