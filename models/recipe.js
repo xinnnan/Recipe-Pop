d1ba3e86af5a9cdab977574ef73b6536f4a3b833
xinnan cao <xinnancao@gmail.com>
2023-01-30 20:45:08 -0500

    fix models

--- models/recipe.js
+++ models/recipe.js
@@ -2,36 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const User = require('./User');
 
-// const Recipe=sequelize.define('Recipe',{
-//     id:{
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     title:{
-//         type:DataTypes.STRING,
-//         allowNull:false
-//     },
-//     ingredients:{
-//         type:DataTypes.TEXT,
-//         allowNull:false
-//     },
-//     instructions:{
-//         type:DataTypes.TEXT,
-//         allowNull:false
-//     },
-//     userID:{
-//         type:DataTypes.INTEGER,
-//         references:{
-//             model:User,
-//             key:'id'
-//         }
-//     }
-
-// });
-const { Model, DataTypes } = require("sequelize");
-
 class Recipe extends Model {}
 
 Recipe.init(
@@ -61,6 +31,11 @@ Recipe.init(
             key:'id',
     },
   },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'recipe',
 });
 
 module.exports = Recipe;
