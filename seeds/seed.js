a433eba14b5dd3c0f346f360d46514e2775d2734
xinnan cao <xinnancao@gmail.com>
2023-01-30 19:09:09 -0500

    added models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
+const { Recipe }=require('../models/recipe');
 
 const userData = require('./userData.json');
+const recipeData = require('./recipeData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
