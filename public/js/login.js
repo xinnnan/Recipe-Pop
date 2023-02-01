5ca7c9e5ddd74f49aaeec6d47e9b9ac85b13ae70
Carlo Caballero <carlopcaballero@gmail.com>
2023-02-01 18:56:20 -0500

    Update Singup

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#email-input').value.trim();
+  const password = document.querySelector('#password-input').value.trim();
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
