84120ba9bd92a35c7a206ab5f4b936f964e4c2e4
Carlo Caballero <carlopcaballero@gmail.com>
2023-01-31 20:48:43 -0500

    Updated signup Backend

---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+const connection = require('connection.js');
+
+const signup = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
+    const username = document.querySelector('#username-input').value.trim();
+    if (err) throw err;
+    console.log("Connected!");
+    var newUser = "INSERT INTO user (name, email, password) VALUES ('" + username + "', '" + email + "', '" + password + "')";
+
+    connection.query(newUser, function (err, result) {
+        if (err) throw err;
+        console.log("User successfully added");
+    });
+};
+
+
+document.querySelector('#signup-submit').addEventListener('click', signup);
