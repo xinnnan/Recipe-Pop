f2cfb042e9d9286a2f6e783201b2ed9feb14b00d
xinnan cao <xinnancao@gmail.com>
2023-02-02 01:27:01 -0500

    added routes

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -2,16 +2,22 @@ const router = require('express').Router();
 const { application } = require('express');
 const {Recipe}  = require('../../models');
 
+
+
 router.get('/:userID', async (req,res)=>{
     const userID=req.params.userID;
     // const query = 'Select * from recipes where userID = 1';
     try {
     const result = await Recipe.findAll({
-        where: {
-          userID: userID,
-        }
+        where: {userID: userID},
       });
-      res.json(result);
+
+
+       res.json(result);
+       console.log(result);
+       
+      //  res.render("userrecipes", {result:recipesData});
+      
     } catch (err) {
         console.log(err);
         res.status(404).json(err);
