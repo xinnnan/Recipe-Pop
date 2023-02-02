bfb8c39f3e6054fd8e4fafddf165f8c94477bc61
xinnan cao <xinnancao@gmail.com>
2023-02-01 21:19:42 -0500

    added few more routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,6 +16,7 @@ router.get('/', withAuth, async (req, res) => {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
